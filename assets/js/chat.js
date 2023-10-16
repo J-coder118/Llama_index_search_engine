a1d306cee2f1db56c4384b6cc8c69611b009a7e1
J-coder118 <nanoweb.7jw@gmail.com>
2023-10-16 13:04:16 -0500

    update and complete

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -24,6 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Activar el primer elemento user-chat inicialmente
-    userChats[0].classList.add('active');
-    chatMessages[0].classList.add('active');
+    // userChats[0].classList.add('active');
+    // chatMessages[0].classList.add('active');
 });
